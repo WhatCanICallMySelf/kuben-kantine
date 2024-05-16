b1fb564663030d2a25d854a59c97af81346387fe
vincent <v1nc3n705@hotmail.com>
2024-05-16 14:46:06 +0200

    admin

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+require_once "db_connection.php";
+$conn = GetDbConnection();
+
+if (isset($_POST["delete"])) {
+    $sql = "DELETE FROM meny WHERE id = ?;";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $_POST["id"]);
+    $stmt->execute();
+    header("Location: test2.php");
+}
